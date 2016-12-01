2330c363e13273ccefd1301dfb11676121e6cdd8
Abdelhakim Bachar <abachar@MacBook-Pro-de-Abdelhakim.local>
2016-12-01 13:48:55 +0100

    Configure karma, mocha, chai and sinon

---
+++ karma.conf.js
@@ -0,0 +1,32 @@
+var webpackConfig = require('./webpack.config');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      'test/**/*.ts'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'test/**/*.ts': ['webpack']
+    },
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve,
+      quiet: true
+    },
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    reporters: ['mocha'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    concurrency: Infinity
+  })
+}
---
+++ test/reducers/index.spec.ts
@@ -0,0 +1,12 @@
+import reducer from '../../src/reducers';
+import { expect } from 'chai';
+
+describe('Reducer', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  it('should fail', () => {
+    expect(true).to.be.equal(true);
+  })
+});
